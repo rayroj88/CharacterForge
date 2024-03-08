612f1ae047d7c62cd5a7afd95256af3de7129408
Ray <xvc3@txstate.edu>
2024-03-08 16:17:04 -0600

    Built react environment. Attempted to start building race page

---
+++ characterforge/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function RaceButton({ race, onClick }) {
+  return (
+    <button className="race-button" data-race={race} >
+      {race}
+    </button>
+  );
+}
+
+function RaceDetails({ details }) {
+  return (
+    <div id="race-details">
+      {details}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Racepage">
+      <h1>Dungeons & Dragons Fifth Edition Races</h1>
+        <div id="race-buttons">
+          {/* Button for each race */}
+          {["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"].map(race => (
+          <RaceButton key={race} race={race} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
