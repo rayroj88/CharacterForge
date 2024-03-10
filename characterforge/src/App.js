9ba4ef8f29b4c87c6dce228bc6be2d57e6e3f9b6
Ray <xvc3@txstate.edu>
2024-03-09 19:46:32 -0600

    Fixed code to be more simple from chatGPTs nonsense.

--- characterforge/src/App.js
+++ characterforge/src/App.js
@@ -1,27 +1,10 @@
-import './App.css';
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
-function RaceButton({ race, onClick }) {
-  return (
-    <button className="race-button" data-race={race} >
-      {race}
-    </button>
-  );
-}
+import Pickrace from './Pickrace';
 
 function App() {
+
   return (
     <div className="App">
-      <div className="Racepage">
-      <h3>Dungeons & Dragons Fifth Edition Races</h3>
-        <div id="race-buttons">
-          {/* Button for each race */}
-          {["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"].map(race => (
-          <RaceButton key={race} race={race} />
-          ))}
-        </div>
-      </div>
+      <Pickrace />
     </div>
   );
 }
---
+++ characterforge/src/Pickrace.js
@@ -0,0 +1,21 @@
+import './Pickrace.css';
+
+const Pickrace = () => {
+
+    return (
+      <div className="Racepage">
+      <h3>Dungeons & Dragons Fifth Edition Races</h3>
+        <button className='race-button' id='human'>Human</button>
+        <button className='race-button' id='half-orc'>Half-Orc</button>
+        <button className='race-button' id='elf'>Elf</button>
+        <button className='race-button' id='half-elf'>Half-Elf</button>
+        <button className='race-button' id='dwarf'>Dwarf</button>
+        <button className='race-button' id='gnome'>Gnome</button>
+        <button className='race-button' id='halfling'>Halfling</button>
+        <button className='race-button' id='dragonborn'>Dragonborn</button>
+        <button className='race-button' id='tiefling'>Tiefling</button>
+      </div>
+    );
+}
+
+export default Pickrace;
