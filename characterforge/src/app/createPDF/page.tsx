375e2b656ab2a4b04e4e70b50c68834873fabed3
Formalitys <jmy40@txstate.edu>
2024-04-14 14:46:27 -0500

    Updated pdf sheet
    Updated PDF sheet

--- characterforge/src/app/createPDF/page.tsx
+++ characterforge/src/app/createPDF/page.tsx
@@ -31,23 +31,64 @@ const calculateStartingHealth = (characterClass, constitutionModifier) => {
 };
 
 const clsHitDie = {
-  barbarian: 'd12',
-  fighter: 'd10',
-  paladin: 'd10',
-  ranger: 'd10',
-  bard: 'd8',
-  cleric: 'd8',
-  druid: 'd8',
-  monk: 'd8',
-  rogue: 'd8',
-  warlock: 'd8',
-  sorcerer: 'd6',
-  wizard: 'd6',
+  Barbarian: 'd12',
+  Fighter: 'd10',
+  Paladin: 'd10',
+  Ranger: 'd10',
+  Bard: 'd8',
+  Cleric: 'd8',
+  Druid: 'd8',
+  Monk: 'd8',
+  Rogue: 'd8',
+  Warlock: 'd8',
+  Sorcerer: 'd6',
+  Wizard: 'd6',
+};
+
+const raceToSpeed = {
+  'Human': '30ft',
+  'Hill Dwarf': '25ft',
+  'Mountain Dwarf': '25ft',
+  'High Elf': '30ft',
+  'Wood Elf': '35ft',
+  'Dark Elf': '30ft', // Drow
+  'Lightfoot Halfling': '25ft',
+  'Stout Halfling': '25ft',
+  'Forest Gnome': '25ft',
+  'Rock Gnome': '25ft',
+  'Half-Elf': '30ft',
+  'Half-Orc': '30ft',
+  'Dragonborn': '30ft',
+  'Tiefling': '30ft',
+  // Add other subraces if necessary
+};
+
+const classSavingThrowProficiencies = {
+  Barbarian: ['strength', 'constitution'],
+  bard: ['dexterity', 'charisma'],
+  Cleric: ['wisdom', 'charisma'],
+  Druid: ['intelligence', 'wisdom'],
+  Fighter: ['strength', 'constitution'],
+  Monk: ['strength', 'dexterity'],
+  Paladin: ['wisdom', 'charisma'],
+  Ranger: ['strength', 'dexterity'],
+  Rogue: ['dexterity', 'intelligence'],
+  Sorcerer: ['constitution', 'charisma'],
+  Warlock: ['wisdom', 'charisma'],
+  Wizard: ['intelligence', 'wisdom'],
 };
 
 const CharacterSheetPage = () => {
   const [characterData, setCharacterData] = useState<any>({});
   const [pdfBytes, setPdfBytes] = useState<Uint8Array | null>(null);
+  const [characterName, setCharacterName] = useState('');
+  const [age, setAge] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [eyeColor, setEyeColor] = useState('');
+  const [skinColor, setSkinColor] = useState('');
+  const [hairColor, setHairColor] = useState('');
+  const [focused, setFocused] = useState('');
 
   useEffect(() => {
     // Retrieve character data from localStorage
@@ -81,6 +122,7 @@ const CharacterSheetPage = () => {
       const score = parseInt(abilityScore, 10); // Ensure the ability score is an integer
       return Math.floor((score - 10) / 2); // Calculate and round down the modifier
     };
+
     // Load the PDF template from the public folder
     const loadPdfTemplate = async () => {
       const pdfTemplateBytes = await fetch('/DnD_5E_CharacterSheet_FormFillable.pdf').then((res) => res.arrayBuffer());
@@ -102,7 +144,16 @@ const CharacterSheetPage = () => {
       const startingHealth = calculateStartingHealth(characterClass, constitution);
       const passiveWisdom = 10 + calculateModifierAsNumber(wisdom);
       const hitDie = clsHitDie[characterClass];
-      
+
+      const name2Field = form.getTextField('CharacterName 2');
+      const ageField = form.getTextField('Age');
+      const heightField = form.getTextField('Height');
+      const weightField = form.getTextField('Weight');
+      const eyeField = form.getTextField('Eyes');
+      const skinField = form.getTextField('Skin');
+      const hairField = form.getTextField('Hair');
+      const speedField = form.getTextField('Speed');
+      const nameField = form.getTextField('CharacterName');
       const hdField = form.getTextField('HD');
       const pasField = form.getTextField('Passive');
       const hpField = form.getTextField('HPCurrent');
@@ -124,6 +175,21 @@ const CharacterSheetPage = () => {
       const wisMod = form.getTextField('WISmod')
       const chaMod = form.getTextField('CHamod')
       const proMod = form.getTextField('ProfBonus')
+      const strST = form.getTextField('ST Strength')
+      const dexST = form.getTextField('ST Dexterity')
+      const conST = form.getTextField('ST Constitution')
+      const intST = form.getTextField('ST Intelligence')
+      const wisST = form.getTextField('ST Wisdom')
+      const chaST = form.getTextField('ST Charisma')
+      
+      nameField.setText(characterName);
+      name2Field.setText(characterName);
+      ageField.setText(age);
+      heightField.setText(height);
+      weightField.setText(weight);
+      eyeField.setText(eyeColor);
+      skinField.setText(skinColor);
+      hairField.setText(hairColor);
 
       if (subrace != null ) {
         raceField.setText(subrace); 
@@ -152,13 +218,18 @@ const CharacterSheetPage = () => {
       maxHPField.setText(startingHealth.toString());
       pasField.setText(passiveWisdom.toString());
       hdField.setText(hitDie);
+
+      const actualRace = (subrace || race); // Use subrace if available, otherwise use race
+      const speed = raceToSpeed[actualRace];
+      
+      speedField.setText(speed);
  
       const modifiedPdfBytes = await pdfDoc.save();
       setPdfBytes(modifiedPdfBytes);
     };
 
     loadPdfTemplate();
-  }, []);
+  }, [characterName, age, height, weight, eyeColor, skinColor, hairColor]);
 
   const downloadPdf = () => {
     if (!pdfBytes) return;
@@ -170,10 +241,69 @@ const CharacterSheetPage = () => {
     link.click();
   };
 
+  const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '20px',
+      background: 'black',
+      color: 'white',
+    },
+    input: {
+      background: 'black',
+      color: 'white',
+      border: '1px solid white',
+      margin: '10px',
+      padding: '10px',
+      width: '200px',
+      borderRadius: '5px',
+      transition: 'border-color 0.3s, box-shadow 0.3s',
+    },
+    inputFocus: {
+      borderColor: '#6200ee',
+      boxShadow: '0 0 5px #6200ee',
+    },
+    button: {
+      fontSize: 'inherit',
+      marginTop: '20px',
+      padding: '10px 20px',
+      background: 'transparent',
+      color: 'white',
+      border: '1px solid white',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      transition: 'background-color 0.3s, color 0.3s',
+    }
+  };
+
+  // Function to generate input fields
+  const createInput = (placeholder, value, setValue) => (
+    <input 
+      style={{...styles.input, ...(focused === placeholder && styles.inputFocus)}} 
+      type="text" 
+      placeholder={placeholder} 
+      value={value} 
+      onChange={e => setValue(e.target.value)} 
+      onFocus={() => setFocused(placeholder)}
+      onBlur={() => setFocused('')}
+    />
+  );
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '20px' }}>
-    {pdfBytes && <button onClick={downloadPdf} style={{ fontSize: 'inherit' }}>Download PDF</button>}
-  </div>
+    <div style={styles.container}>
+      {createInput("Character Name", characterName, setCharacterName)}
+      {createInput("Age", age, setAge)}
+      {createInput("Height", height, setHeight)}
+      {createInput("Weight", weight, setWeight)}
+      {createInput("Eye Color", eyeColor, setEyeColor)}
+      {createInput("Skin Tone", skinColor, setSkinColor)}
+      {createInput("Hair Color", hairColor, setHairColor)}
+      
+      <button style={styles.button} onClick={() => downloadPdf()}>Download PDF</button>
+    </div>
   );
 };
 
